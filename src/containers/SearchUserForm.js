9eaf0d0c75b4f962b9a3a17a86aaa7b46232405e
effel <rijik@list.ru>
2019-11-24 22:41:27 +0100

    Adding ans setting up all components

---
+++ src/containers/SearchUserForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+          gjhgjh
+        </div>
+    );
+};
+
+export default App;
